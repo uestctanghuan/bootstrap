23565e770dfef41e184d59988119bb6d6cc4289f
Johann-S <johann.servoire@gmail.com>
2019-06-30 11:37:25 +0200

    wip: rewrite scrollspy unit tests

---
+++ js/src/scrollspy/scrollspy.spec.js
@@ -0,0 +1,28 @@
+import ScrollSpy from './scrollspy'
+
+/** Test helpers */
+import { getFixture, clearFixture } from '../../tests/helpers/fixture'
+
+describe('ScrollSpy', () => {
+  let fixtureEl
+
+  beforeAll(() => {
+    fixtureEl = getFixture()
+  })
+
+  afterEach(() => {
+    clearFixture()
+  })
+
+  describe('VERSION', () => {
+    it('should return plugin version', () => {
+      expect(ScrollSpy.VERSION).toEqual(jasmine.any(String))
+    })
+  })
+
+  describe('Default', () => {
+    it('should return plugin default config', () => {
+      expect(ScrollSpy.Default).toEqual(jasmine.any(Object))
+    })
+  })
+})
